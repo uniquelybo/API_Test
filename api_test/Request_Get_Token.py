2182bdc2507648e0e7695400977a07deb0acda4a
uniquelybo <uniquelybo@163.com>
2020-09-28 21:03:48 +0800

    更新token

--- api_test/Request_Get_Token.py
+++ api_test/Request_Get_Token.py
@@ -2,10 +2,11 @@ import requests
 
 
 def get_token():
+    print("获取token.........")
     data = {"name": "123", "password": "123456"}
     login_url = "http://192.168.16.225:8788/api/gs/client/number/login/123/123456"
     response = requests.get(url=login_url, params=data)
     # print(response)
     token = response.json()['loginResponse']['token']
-    # print(token)
+    print(token)
     return token
