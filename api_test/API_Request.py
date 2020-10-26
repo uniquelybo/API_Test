cdbc2b2a10f1f125049f474512772b440848662f
renxiaobo <renxiaobo@163.com>
2020-10-26 10:26:37 +0800

    update

--- api_test/API_Request.py
+++ api_test/API_Request.py
@@ -3,45 +3,80 @@ import pandas as pd
 import json
 
 class API_Request():
-    def __init__(self, api_data, token):
+    def __init__(self, api_data):
         self.url = api_data['url']
         self.method = api_data['method']
         self.data = api_data['data']
+        self.json_data = api_data['json']
         self.headers = api_data['headers']
-        self.token = token
+        # self.token = token
         # print(api_data)
 
     def request(self):
         if self.method == "POST":
             if pd.isnull(self.headers):
                 if pd.isnull(self.data):
-                    response = requests.post(url=self.url)
+                    if pd.isnull(self.json_data):
+                        response = requests.post(url=self.url)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, json=self.json_data)
                 else:
-                    self.data = json.loads(self.data)
-                    response = requests.post(url=self.url, data=self.data)
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.post(url=self.url, data=self.data)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, data=self.data, json=self.json_data)
             else:
                 self.headers = json.loads(self.headers)
-                self.headers['token'] = self.token
                 if pd.isnull(self.data):
-                    response = requests.post(url=self.url, headers=self.headers)
+                    if pd.isnull(self.json_data):
+                        response = requests.post(url=self.url, headers=self.headers)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, json=self.json_data, headers=self.headers)
                 else:
-                    self.data = json.loads(self.data)
-                    response = requests.post(url=self.url, data=self.data, headers=self.headers)
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.post(url=self.url, data=self.data, headers=self.headers)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, data=self.data, json=self.json_data, headers=self.headers)
 
         elif self.method == "GET":
             if pd.isnull(self.headers):
                 if pd.isnull(self.data):
-                    response = requests.get(url=self.url)
+                    if pd.isnull(self.json_data):
+                        response = requests.get(url=self.url)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, json=self.json_data)
                 else:
-                    self.data = json.loads(self.data)
-                    response = requests.get(url=self.url, params=self.data)
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.get(url=self.url, params=self.data)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, params=self.data, json=self.json_data)
             else:
                 self.headers = json.loads(self.headers)
-                self.headers['token'] = self.token
                 if pd.isnull(self.data):
-                    response = requests.get(url=self.url, headers=self.headers)
+                    if pd.isnull(self.json_data):
+                        response = requests.get(url=self.url, headers=self.headers)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, json=self.json_data, headers=self.headers)
                 else:
-                    self.data = json.loads(self.data)
-                    response = requests.get(url=self.url, params=self.data, headers=self.headers)
-        # print(self.headers)
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.get(url=self.url, params=self.data, headers=self.headers)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, params=self.data, json=self.json_data,
+                                                 headers=self.headers)
         return response
--- api_test/Read_API_Data.py
+++ api_test/Read_API_Data.py
@@ -14,7 +14,7 @@ class Read_API_Data():
         self.log_file.write(str(datetime.datetime.now()) + "\n")
         self.log_file.write("-------------------------------------------------------------------" + "\n")
         self.log_file.write("共有%d条数据" % self.api_num + '\n')
-        self.token = Request_Get_Token.get_token()
+        # self.token = Request_Get_Token.get_token()
 
     def execute_api(self):
         # 对数据进行逐条发送请求并接收结果
@@ -25,9 +25,10 @@ class Read_API_Data():
             url = self.api_file['URL'][list_num - 1]
             method = self.api_file['Method'][list_num - 1]
             headers = self.api_file['Header'][list_num - 1]
+            json_data = self.api_file['json'][list_num - 1]
             url = host + url
-            api_data = {"url": url, "method": method, "data": data, "headers": headers}
-            api_request = API_Request(api_data, self.token)
+            api_data = {"url": url, "method": method, "data": data, "json": json_data, "headers": headers}
+            api_request = API_Request(api_data)
             response = api_request.request()
             print(response.url)
             print(response.text)
